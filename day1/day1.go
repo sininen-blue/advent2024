25a2c126215cce6c6124b4a9aa0158d962d66a89
sininen-blue <108949710+sininen-blue@users.noreply.github.com>
2024-12-02 15:40:18 +0800

    day1 part 1

--- day1/day1.go
+++ day1/day1.go
@@ -1,26 +1,60 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"math"
 	"os"
+	"slices"
+	"strconv"
 	"strings"
 )
 
+// plan
+// simplest is to just pop out the smallest and do that a bunch
+// but doing that with slices
+// or maybe get a list of
+
+// im stupid, just sort both lists and get the sums lmao
+
 func main() {
-	content, err := os.ReadFile("input.txt")
+	file, err := os.Open("input.txt")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+	defer file.Close()
 
-	input := string(content)
-
-	inputSlice := strings.Split(input, "\n")
-
-	var leftList []string
-	var rightList []string
-	for item := range inputSlice {
+	var inputSlice []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		inputSlice = append(inputSlice, scanner.Text())
 	}
 
-	fmt.Println(tabsplit)
+	var leftList []int
+	var rightList []int
+	for _, row := range inputSlice {
+		items := strings.Split(row, "   ")
+
+		leftInt, err := strconv.Atoi(items[0])
+		rightInt, err := strconv.Atoi(items[1])
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		leftList = append(leftList, leftInt)
+		rightList = append(rightList, rightInt)
+	}
+
+	slices.Sort(leftList)
+	slices.Sort(rightList)
+
+	var sum int
+	for i := range inputSlice {
+		difference := math.Abs(float64(leftList[i] - rightList[i]))
+		sum += int(difference)
+	}
+
+	fmt.Println(sum)
 }
