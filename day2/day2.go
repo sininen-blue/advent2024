4199dd5f760eb890dac919b3ee0b66dead1e9008
sininen-blue <108949710+sininen-blue@users.noreply.github.com>
2024-12-02 18:26:03 +0800

    day 2 part 2 second attempt

--- day2/day2.go
+++ day2/day2.go
@@ -26,59 +26,83 @@ func main() {
 	count := 0
 	for _, row := range inputSlice {
 		rowSlice := strings.Split(row, " ")
+		safe, dampenedSlice := checkSafety(rowSlice)
 
-		safe := true
-		prevLevel := -1
-		state := ""
-		problems := 0
-		for _, levelStr := range rowSlice {
-			level, err := strconv.Atoi(levelStr)
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-
-			// skip first element
-			if prevLevel == -1 {
-				prevLevel = level
-				continue
-			}
-
-			// check whether increasing or decreasing
-			difference := level - prevLevel
-			prevLevel = level
-			curState := state
-			if difference < 0 {
-				state = "dec"
-			} else {
-				state = "inc"
-			}
-
-			// check unsafe
-			absDiff := math.Abs(float64(difference))
-			if absDiff > 3 || absDiff <= 0 {
-				// checks if levels reach thresholds
-				problems += 1
-				continue
-			}
-
-			if curState != state && curState != "" {
-				// checks if it stops increasing or decreasing
-				problems += 1
-
-				// keep state the same if problem occurs
-				state = curState
-				continue
-			}
-
-			if problems > 1 {
-				safe = false
-				break
-			}
+		if len(dampenedSlice) != 0 {
+			safe, _ = checkSafety(dampenedSlice)
 		}
-		if safe == true {
+
+		if safe {
 			count += 1
+		} else {
 		}
 	}
 	fmt.Println(count)
+}
+
+func checkSafety(rowSlice []string) (bool, []string) {
+	dampenedSlice := []string{}
+
+	safe := true
+	prevLevel := -1
+	state := ""
+	problems := 0
+	for i, levelStr := range rowSlice {
+		level, err := strconv.Atoi(levelStr)
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+
+		// skip first element
+		if prevLevel == -1 {
+			prevLevel = level
+			continue
+		}
+
+		// check whether increasing or decreasing
+		difference := level - prevLevel
+		prevLevel = level
+		curState := state
+		if difference < 0 {
+			state = "dec"
+		} else {
+			state = "inc"
+		}
+
+		// check unsafe
+		absDiff := math.Abs(float64(difference))
+		if absDiff > 3 || absDiff <= 0 {
+			// checks if levels reach thresholds
+			problems += 1
+
+			if len(dampenedSlice) == 0 {
+				dampenedSlice = remove(rowSlice, i)
+			}
+			continue
+		}
+
+		if curState != state && curState != "" {
+			// checks if it stops increasing or decreasing
+			problems += 1
+			if len(dampenedSlice) == 0 {
+				dampenedSlice = remove(rowSlice, i)
+			}
+
+			// keep state the same if problem occurs
+			state = curState
+			continue
+		}
+
+		if problems > 1 {
+			safe = false
+			break
+		}
+	}
+
+	return safe, dampenedSlice
+}
+
+func remove(slice []string, s int) []string {
+	return append(slice[:s], slice[s+1:]...)
 }
