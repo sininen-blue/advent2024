3dc11781ad207ddfebb9a901b182ae0267dfb155
sininen-blue <108949710+sininen-blue@users.noreply.github.com>
2024-12-02 18:50:05 +0800

    day 2 part 1 cleaned

--- day2/day2.go
+++ day2/day2.go
@@ -10,91 +10,49 @@ import (
 )
 
 func main() {
-	file, err := os.Open("input.txt")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer file.Close()
-
-	var inputSlice []string
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		inputSlice = append(inputSlice, scanner.Text())
-	}
+	inputSlice := getInput("input.txt")
 
 	count := 0
 	for _, row := range inputSlice {
 		rowSlice := strings.Split(row, " ")
-		safe, dampenedSlice := checkSafety(rowSlice)
-
-		if len(dampenedSlice) != 0 {
-			safe, _ = checkSafety(dampenedSlice)
-		}
+		safe, _ := checkSafety(rowSlice)
 
 		if safe {
 			count += 1
-		} else {
 		}
 	}
 	fmt.Println(count)
 }
 
 func checkSafety(rowSlice []string) (bool, []string) {
-	dampenedSlice := []string{}
-
 	safe := true
-	prevLevel := -1
+	dampenedSlice := []string{}
+	row := intSlice(rowSlice)
+
+	prevItem := row[0]
+	prevState := ""
 	state := ""
-	problems := 0
-	for i, levelStr := range rowSlice {
-		level, err := strconv.Atoi(levelStr)
-		if err != nil {
-			fmt.Println(err)
+	for _, item := range row[1:] {
+		diff := item - prevItem
+		prevItem = item
+		prevState = state
+		if diff == 0 {
+			state = "flat"
+			safe = false
 			break
-		}
-
-		// skip first element
-		if prevLevel == -1 {
-			prevLevel = level
-			continue
-		}
-
-		// check whether increasing or decreasing
-		difference := level - prevLevel
-		prevLevel = level
-		curState := state
-		if difference < 0 {
+		} else if diff < 0 {
 			state = "dec"
-		} else {
+		} else if diff > 0 {
 			state = "inc"
 		}
 
-		// check unsafe
-		absDiff := math.Abs(float64(difference))
+		if prevState != state && prevState != "" {
+			safe = false
+			break
+		}
+
+		absDiff := math.Abs(float64(diff))
 		if absDiff > 3 || absDiff <= 0 {
-			// checks if levels reach thresholds
-			problems += 1
-
-			if len(dampenedSlice) == 0 {
-				dampenedSlice = remove(rowSlice, i)
-			}
-			continue
-		}
-
-		if curState != state && curState != "" {
-			// checks if it stops increasing or decreasing
-			problems += 1
-			if len(dampenedSlice) == 0 {
-				dampenedSlice = remove(rowSlice, i)
-			}
-
-			// keep state the same if problem occurs
-			state = curState
-			continue
-		}
-
-		if problems > 1 {
 			safe = false
 			break
 		}
@@ -103,6 +61,36 @@ func checkSafety(rowSlice []string) (bool, []string) {
 	return safe, dampenedSlice
 }
 
+func getInput(path string) []string {
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	var output []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		output = append(output, scanner.Text())
+	}
+
+	return output
+}
+
 func remove(slice []string, s int) []string {
 	return append(slice[:s], slice[s+1:]...)
+}
+
+func intSlice(slice []string) []int {
+	var output []int
+	for _, item := range slice {
+		x, err := strconv.Atoi(item)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		output = append(output, x)
+	}
+
+	return output
 }
