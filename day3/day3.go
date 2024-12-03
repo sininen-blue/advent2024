0a14300c0d2536072d0d91df9995d104b9e588c8
sininen-blue <108949710+sininen-blue@users.noreply.github.com>
2024-12-03 19:19:44 +0800

    day 3 part 1 attempt

--- day3/day3.go
+++ day3/day3.go
@@ -4,18 +4,89 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 )
+
+type Command struct {
+	x int
+	y int
+}
 
 func main() {
 	inputSlice := getInput("input.txt")
 
-	for _, i := range inputSlice {
-		fmt.Println(i)
-		fmt.Println()
-		fmt.Println()
-		fmt.Println()
-		fmt.Println()
+	commands := []Command{}
+	for _, line := range inputSlice {
+		// mul(,)
+		count := 0
+		for _, char := range line {
+			inputX := ""
+			inputY := ""
+			switch count {
+			case 0:
+				if string(char) == "m" {
+					count += 1
+				} else {
+					count = 0
+				}
+			case 1:
+				if string(char) == "u" {
+					count += 1
+				} else {
+					count = 0
+				}
+			case 2:
+				if string(char) == "l" {
+					count += 1
+				} else {
+					count = 0
+				}
+			case 3:
+				if string(char) == "(" {
+					count += 1
+				} else {
+					count = 0
+				}
+			case 4:
+				if string(char) == "," && inputX != "" {
+					inputX = ""
+					count += 1
+				} else {
+					count = 0
+				}
+
+				if _, err := strconv.Atoi(string(char)); err == nil {
+					inputX = inputX + string(char)
+					fmt.Println(inputX)
+				} else {
+					count = 0
+				}
+			case 5:
+				if string(char) == ")" && inputY != "" {
+					inputY = ""
+					x, err := strconv.Atoi(inputX)
+					y, err := strconv.Atoi(inputY)
+					if err != nil {
+						fmt.Println(err)
+						return
+					}
+					command := Command{x: x, y: y}
+					commands = append(commands, command)
+
+					count = 0
+				} else {
+					count = 0
+				}
+
+				if _, err := strconv.Atoi(string(char)); err == nil {
+					inputY = inputY + string(char)
+				} else {
+					count = 0
+				}
+			}
+		}
 	}
+	fmt.Println(commands)
 }
 
 func getInput(path string) []string {
